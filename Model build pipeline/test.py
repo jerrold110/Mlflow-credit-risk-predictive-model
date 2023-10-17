d4180fa55d80babbb61d35b979391d2e1f3fc617
Jerrold Koh <jerroldfisher@live.com>
2023-10-17 17:18:30 +0800

    Prototype models

---
+++ Model build pipeline/test.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import pandas as pd
+from Feature_extractor import feature_extractor_pipeline
+
+# base_directory = os.path.dirname(os.path.dirname(__file__))
+# sys.path.append(f"{base_directory}")
+
+# data = pd.read_csv('Data/credit.csv')
+# data_fe = feature_extractor_pipeline(data)
+# print(data_fe.columns)
--- README.md
+++
@@ -1,2 +0,0 @@
-# mlflow-credit-risk-predictive-model
- Tracking, packaging, and deploying redit risk predictive model with MLflow lifecycle framework v 2.7.1. The artifacts and data of this repository are stored in the default path mlruns inthis project, instead of on a server
